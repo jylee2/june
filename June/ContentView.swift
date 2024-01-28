da794186ba8d1fdea5aeeea89c1eaab3e76b447e
Jun Yi Lee <leej1025y@live.com>
2024-01-28 15:43:07 +1100

    Initial Commit

---
+++ June/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  June
+//
+//  Created by Jun Yi Lee on 28/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ June/JuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuneApp.swift
+//  June
+//
+//  Created by Jun Yi Lee on 28/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct JuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
